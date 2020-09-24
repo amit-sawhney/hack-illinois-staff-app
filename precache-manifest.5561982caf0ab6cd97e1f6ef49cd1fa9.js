b1f93a7b790ec8e69925e67c99855d23f07eed33
Amit Sawhney <amit.sawhney2002@gmail.com>
2020-09-24 12:48:36 -0500

    Updates

---
+++ precache-manifest.5561982caf0ab6cd97e1f6ef49cd1fa9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1080a976ce6eb593abe8833d7844098c",
+    "url": "/hack-illinois-staff-app/index.html"
+  },
+  {
+    "revision": "4e7a041ced861c54d1d7",
+    "url": "/hack-illinois-staff-app/static/css/main.d3f8c0c2.chunk.css"
+  },
+  {
+    "revision": "84ec6eb279d3315af869",
+    "url": "/hack-illinois-staff-app/static/js/2.e7b1afec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hack-illinois-staff-app/static/js/2.e7b1afec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7a041ced861c54d1d7",
+    "url": "/hack-illinois-staff-app/static/js/main.cacc7e30.chunk.js"
+  },
+  {
+    "revision": "28d2a3d9e9accb94f6d7",
+    "url": "/hack-illinois-staff-app/static/js/runtime-main.be0f3803.js"
+  },
+  {
+    "revision": "7798c8e56acee457c7750d703bd0015e",
+    "url": "/hack-illinois-staff-app/static/media/defaultIcon.7798c8e5.svg"
+  },
+  {
+    "revision": "2e876477e59a22bda097fc329aeb1cf8",
+    "url": "/hack-illinois-staff-app/static/media/mealIcon.2e876477.svg"
+  },
+  {
+    "revision": "a09af525614a5552aca823a03a7cdf94",
+    "url": "/hack-illinois-staff-app/static/media/miniEvent.a09af525.svg"
+  },
+  {
+    "revision": "c04ed54783e798b294ef1e2588053349",
+    "url": "/hack-illinois-staff-app/static/media/speakerIcon.c04ed547.svg"
+  },
+  {
+    "revision": "69524b381c730727ac01261598f4509c",
+    "url": "/hack-illinois-staff-app/static/media/workshopIcon.69524b38.svg"
+  }
+]);
